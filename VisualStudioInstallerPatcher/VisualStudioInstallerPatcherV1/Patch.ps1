e44f1641945a9d395d4c3ef269166bc933227659
Jon Edwards <jon@thesource.co.uk>
2019-04-01 15:29:52 +0100

    now updates all .vdproj files in a given directory

--- VisualStudioInstallerPatcher/VisualStudioInstallerPatcherV1/Patch.ps1
+++ VisualStudioInstallerPatcher/VisualStudioInstallerPatcherV1/Patch.ps1
@@ -4,12 +4,8 @@ param
     [string]
     $Sourcefolder = (Get-VstsInput -Name 'Sourcefolder' -Require),
 
-    [Parameter()]
-    [string]
-    $Sourcefiles = (Get-VstsInput -Name 'Sourcefiles' -Require),
-
 	[Parameter()]
-    [string]
+    [version]
     $Version = (Get-VstsInput -Name 'Version' -Require)
 )
 Trace-VstsEnteringInvocation $MyInvocation
@@ -18,14 +14,14 @@ If ($DeploymentType -eq 'Agent')
 {
     
 }
-$scriptBlock = {
-    $file = $args[0]
-    Write-Output "Patching [$filename]"	
+
+Function Patch-File
+{
+	param([string]$filepath)
+
+	Write-Output "Patching $filepath"
 
 	Try{
-		$filename = Join-Path -Path $Sourcefolder -ChildPath $file -Resolve
-
-
 		$productCodePattern     = '\"ProductCode\" = \"8\:{([\d\w-]+)}\"'
 		$packageCodePattern     = '\"PackageCode\" = \"8\:{([\d\w-]+)}\"'
 		$productVersionPattern  = '\"ProductVersion\" = \"8\:[0-9]+(\.([0-9]+)){1,3}\"'
@@ -33,24 +29,34 @@ $scriptBlock = {
 		$packageCode            = '"PackageCode" = "8:{' + [guid]::NewGuid().ToString().ToUpper() + '}"'
 		$productVersion         = '"ProductVersion" = "8:' + $Version.ToString(3) + '"'
 
-		(Get-Content $filename) | ForEach-Object {
+		(Get-Content $filepath) | ForEach-Object {
 			% {$_ -replace $productCodePattern, $productCode } |
 			% {$_ -replace $packageCodePattern, $packageCode } |
 			% {$_ -replace $productVersionPattern, $productVersion }
-		} | Set-Content $filename
+		} | Set-Content $filepath
 	}
 	Catch{
-		Write-Error "Unable to patch [$filename] confirm the vdproj file is present." -ErrorAction Stop
+		Write-Error "Unable to patch [$filepath] confirm the vdproj file is present." -ErrorAction Stop
 	}
 }
 
-$invokeCommandSplat = @{
-    ScriptBlock = $scriptBlock
-}
-If($credential)
+Function Start-Patching
 {
-    $invokeCommandSplat.Credential = $credential
-    $invokeCommandSplat.ComputerName = $machines
+	$regex = "\d{1,2}.\d{1,2}.\d{1,4}"
+
+	If(!($Version -match $regex))
+	{
+		Write-Error "Version number is invalid, must be of format '##.##.####'" -ErrorAction Stop
+	}
+
+    Write-Output "Starting patching for source folder [$Sourcefolder]"
+
+    $files = Get-Childitem -Path $Sourcefolder -Include *.vdproj -Recurse		
+
+	ForEach ($file in $files) {        
+		Patch-File -filepath $file
+	}
 }
-Invoke-Command @invokeCommandSplat -ArgumentList $Sourcefiles
+
+Start-Patching
 Trace-VstsLeavingInvocation $MyInvocation
